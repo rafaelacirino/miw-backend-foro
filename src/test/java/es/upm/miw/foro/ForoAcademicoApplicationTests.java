37d4505e19a3fe0434f9ff842dceea3bf7f94290
rafaelacirino <rafaelaborbaf@gmail.com>
2025-02-22 10:11:32 +0100

    Project configuration

---
+++ src/test/java/es/upm/miw/foro/ForoAcademicoApplicationTests.java
@@ -0,0 +1,14 @@
+package es.upm.miw.foro;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class ForoAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
