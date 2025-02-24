893639453e736d0dec009df4dce59a386818d96f
rafaelacirino <rafaelaborbaf@gmail.com>
2025-02-24 21:22:27 +0100

    test endpoint render #9

--- src/main/java/es/upm/miw/foro/api/controller/UserController.java
+++ src/main/java/es/upm/miw/foro/api/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/")
 public class UserController {
 
     private final UserService userService;
