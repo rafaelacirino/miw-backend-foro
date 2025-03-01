994fcf6c993a09c9c5bb7809e021dca0cdc476b4
rafaelacirino <rafaelaborbaf@gmail.com>
2025-03-01 21:27:09 +0100

    sonar test #11

--- src/test/java/es/upm/miw/foro/api/controller/UserControllerTest.java
+++ src/test/java/es/upm/miw/foro/api/controller/UserControllerTest.java
@@ -9,7 +9,6 @@ import es.upm.miw.foro.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
