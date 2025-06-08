c7439b0874d9081be1a9d0e38417137b40e342e8
rafaelacirino <rafaelaborbaf@gmail.com>
2025-06-08 13:51:30 +0200

    change role lauraye27/miw-frontend-foro#29

--- src/main/java/es/upm/miw/foro/persistence/model/Role.java
+++ src/main/java/es/upm/miw/foro/persistence/model/Role.java
@@ -2,11 +2,5 @@ package es.upm.miw.foro.persistence.model;
 
 public enum Role {
 
-    ADMIN, MEMBER, UNKNOWN;
-
-    public static final String PREFIX = "ROLE_";
-
-    public static Role of(String withPrefix) {
-        return Role.valueOf(withPrefix.replace(Role.PREFIX, ""));
-    }
+    ADMIN, MEMBER, UNKNOWN
 }
